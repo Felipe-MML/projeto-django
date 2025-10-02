8e910e075c27a3001ed075689caa6f163d05c2d7
Felipe-MML <felipemaras20@gmail.com>
2025-10-02 16:39:30 -0300

    Criando o headers e adicionando o head na página no partials

--- recipes/views.py
+++ recipes/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 # HTTP REQUEST
 def home(request):
-    return render(request, 'recipes/home.html', context={
+    return render(request, 'recipes/pages/home.html', context={
         'name': 'Felipe'
     })
